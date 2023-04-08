3f83fd3df4b815a562fdd9762be0366069b1a675
Jesse <jsegall7@gmail.com>
2023-04-08 19:50:19 -0400

    Controller updates

--- src/questions/questions.controller.ts
+++ src/questions/questions.controller.ts
@@ -1,20 +1,22 @@
 import { AppDataSource } from '../..';
 import { Question } from './questions.entity';
 import { instanceToPlain } from 'class-transformer';
+import { Request, Response } from 'express';
+import { validationResult } from 'express-validator/src/validation-result';
 
-export class QuestionsController {
-  constructor(
-    private questionRepository = AppDataSource.getRepository(
-      Question,
-    ),
-  ) {}
-
-  public async getAll(): Promise<Question[]> {
+class QuestionsController {
+  // Method for Get route
+  public async getAll(
+    req: Request,
+    res: Response,
+  ): Promise<Response> {
     //Declare variable to hold all questions
     let allQuestions: Question[];
     //Fetch all tasks using repo
     try {
-      allQuestions = await this.questionRepository.find({
+      allQuestions = await AppDataSource.getRepository(
+        Question,
+      ).find({
         order: {
           date: 'ASC',
         },
@@ -25,9 +27,57 @@ export class QuestionsController {
         allQuestions,
       ) as Question[];
 
-      return allQuestions;
+      return res.json(allQuestions).status(200);
+    } catch (_error) {
+      return res
+        .json({ error: 'Internal Server Error' })
+        .status(500);
+    }
+  }
+  //Method for POST route
+  public async create(
+    req: Request,
+    res: Response,
+  ): Promise<Response> {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .json({ errors: errors.array() });
+    }
+
+    //Create new instance of question
+    const newQuestion = new Question();
+
+    //Add required properties to the Question Object
+    newQuestion.title = req.body.title;
+    newQuestion.date = req.body.date;
+    newQuestion.description = req.body.description;
+    newQuestion.difficulty = req.body.difficulty;
+    newQuestion.status = req.body.status;
+
+    //Add the new Question to the DB
+    let createdQuestion: Question;
+
+    try {
+      createdQuestion = await AppDataSource.getRepository(
+        Question,
+      ).save(newQuestion);
+
+      // Convert question instance to an object
+
+      createdQuestion = instanceToPlain(
+        createdQuestion,
+      ) as Question;
+
+      return res.json(createdQuestion).status(201);
     } catch (error) {
-      console.log(error);
+      return res
+        .status(400)
+        .json({ errors: errors.array() });
     }
   }
 }
+
+export const questionController = new QuestionsController();
